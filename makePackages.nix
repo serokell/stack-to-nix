86fdacbf7e82d5ff3cc5e366db6b73c00251a15e
Kirill Elagin <kirelagin@gmail.com>
2018-06-15 21:39:46 +0300

    Disable tests for all dependencies

--- makePackages.nix
+++ makePackages.nix
@@ -6,7 +6,7 @@ let
   };
 
   inherit (builtins) getAttr;
-  inherit (pkgs.haskell.lib) dontCheck;
+  inherit (pkgs.haskell.lib) doCheck;
   inherit (pkgs.lib) cleanSource composeExtensions foldr mapAttrs;
   inherit (import ./stackage.nix) fixResolverName;
   inherit (stackagePackages) callHackage callCabal2nix;
@@ -26,16 +26,19 @@ let
 
   stackagePackages = projPkgs.haskell.packages.stackage."${resolver}".override {
     overrides = foldr composeExtensions (_:_:{}) [
+      doNotCheckDeps
       extra-deps
       local-packages
       overrides
     ];
   };
 
-  resolveExtraDepNoCheck = self: super: name: spec:
-    dontCheck (resolveExtraDep self super name spec);
+  doNotCheckDeps = self: super: {
+    mkDerivation = drv: super.mkDerivation (drv // { doCheck = false; });
+  };
+
   extra-deps = self: super:
-    mapAttrs (resolveExtraDepNoCheck self super) proj.extra-deps;
+    mapAttrs (resolveExtraDep self super) proj.extra-deps;
 
   resolvedPackages =
     mapAttrs (_: path: proj.root + ("/" + path)) proj.packages;
@@ -45,7 +48,7 @@ let
       fakeLicense = pkg: pkg.overrideAttrs (old: {
           meta = old.meta // { license = pkgs.lib.licenses.free; };
       });
-    in fakeLicense (callCabal2nix name (cleanSource path) {});
+    in fakeLicense (doCheck (callCabal2nix name (cleanSource path) {}));
   local-packages = self: super:
     mapAttrs mkLocalPackage resolvedPackages;
 
