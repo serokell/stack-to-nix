5b9bab51df84cab76de4d18d8474f675395bc0b0
Kirill Elagin <kirelagin@gmail.com>
2018-08-07 19:58:56 +0300

    NXG-11 Use default nixpkgs for Nixage itself

--- hs/project.nix
+++ hs/project.nix
@@ -1,9 +1,5 @@
 {
   resolver = "lts-11.14";
-  nixpkgs = {
-    url = "https://github.com/nixos/nixpkgs/archive/14a9ca27e69e33ac8ffb708de08883f8079f954a.tar.gz";
-    sha256 = "1grsq8mcpl88v6kz8dp0vsybr0wzfg4pvhamj42dpd3vgr93l2ib";
-  };
   packages = { nixage = "."; };
   extra-deps = {
     aeson-options = "0.0.0";
