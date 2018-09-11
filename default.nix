21009934b5e524738a924d4d0667b7def47dd255
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-11 13:46:25 +0000

    Bump nixpkgs-stackage to latest master

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {}
 , overrides ? (_: _: {})
-, stackage ? (fetchGit { url = "https://github.com/typeable/nixpkgs-stackage"; rev = "6f7a24dbdb8086144cad7a55363e1ec04214b748"; }) }:
+, stackage ? (fetchGit { url = "https://github.com/typeable/nixpkgs-stackage"; rev = "7c94ec65e12850e7dd5282e83a6da91f88cb8857"; }) }:
 
 let
   lib = import ./lib.nix pkgs;
